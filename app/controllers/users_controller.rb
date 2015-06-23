d50c8530c9eaa49223949d84f7f8349501893811
Miriam Moser <mirjoy.moser@gmail.com>
2015-06-23 16:18:54 -0600

    added languages to seed data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
 class UsersController < ApplicationController
-
+  def edit
+    @languages = Language.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seeds
+  def self.create
+    add_languages
+  end
+
+  def self.add_languages
+    languages = %w[JavaScript Java Ruby C CSS PHP Python C++
+      Objective-C C# Shell R CoffeeScript Go Perl Scala VimL
+      Clojure Haskell Erlang Rust Swift]
+
+    languages.each do |l|
+      Language.create(name: l)
+    end
+  end
+end
+
+Seeds.create
