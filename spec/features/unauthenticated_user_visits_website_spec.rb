0b6c3fedfc43fe3a17575f7ce8ea1bdc31425f31
Miriam Moser <mirjoy.moser@gmail.com>
2015-06-23 11:38:25 -0600

    added buttons to front page for signing in and creating account

--- spec/features/unauthenticated_user_visits_website_spec.rb
+++ spec/features/unauthenticated_user_visits_website_spec.rb
@@ -3,6 +3,10 @@ require "rails_helper"
 RSpec.describe "unauthenticated user visits home page", type: :feature do
   it "can see the page" do
     visit root_path
-    expect(page).to have_content("Pairr")
+    expect(page).to have_content("Find Your Pair")
+    expect(page).to have_content("Create An Account")
+
+    click_link_or_button("Start Pairing")
+    expect(current_path).to eq(new_session_path)
   end
 end
