8dc7a3ff4fd30d8fa8ba5d82ee27c65a068c4159
Miriam Moser <mirjoy.moser@gmail.com>
2015-06-23 23:06:22 -0600

    added language validation

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Language, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "must have a name" do
+    lang1 = Language.create(name: "catlang")
+    lang2 = Language.create(name: "")
+
+    expect(lang1.id).to be_truthy
+    expect(lang2.id).to be_falsy
+  end
 end
